4bf0f3dd0d4421b70a06f1f069eb50252a0ee050
Mohan Li <mohanli9807@gmail.com>
2025-05-03 18:36:18 +0800

    Update split_fn_trDR_teND.py

--- split_fn_trDR_teND.py
+++ split_fn_trDR_teND.py
@@ -379,16 +379,16 @@ def split_trDR_teND_Py(
         cur_train_label = cur_train_label[permuted_indices]
 
         # Test scaling (same as train)
-        # indices = torch.randint(0, cur_test_label.shape[0],
-        #                         (int(cur_test_label.shape[0] * (DA_dataset_scaling - 1)),))
-        # sampled_data = cur_test_feature[indices]
-        # sampled_label = cur_test_label[indices]
+        indices = torch.randint(0, cur_test_label.shape[0],
+                                (int(cur_test_label.shape[0] * (DA_dataset_scaling - 1)),))
+        sampled_data = cur_test_feature[indices]
+        sampled_label = cur_test_label[indices]
 
-        # cur_test_feature = torch.cat((cur_test_feature, sampled_data), dim=0)
-        # cur_test_label = torch.cat((cur_test_label, sampled_label), dim=0)
-        # permuted_indices = torch.randperm(cur_test_label.shape[0])
-        # cur_test_feature = cur_test_feature[permuted_indices]
-        # cur_test_label = cur_test_label[permuted_indices]
+        cur_test_feature = torch.cat((cur_test_feature, sampled_data), dim=0)
+        cur_test_label = torch.cat((cur_test_label, sampled_label), dim=0)
+        permuted_indices = torch.randperm(cur_test_label.shape[0])
+        cur_test_feature = cur_test_feature[permuted_indices]
+        cur_test_label = cur_test_label[permuted_indices]
 
 
         # generate drifting
